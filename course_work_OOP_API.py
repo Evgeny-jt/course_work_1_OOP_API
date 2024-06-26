f3d12a5800ff6cf308cc8af4a212b893b5b3e942
Evgeny Troshin <dylan_jt@mail.ru>
2024-06-26 14:00:48 +0300

    revision

--- course_work_OOP_API.py
+++ course_work_OOP_API.py
@@ -34,22 +34,21 @@ class VKAPIClient:
         photos = {}
         url_photo = ''
         like_photo = ''
-        n = 0
 
-        for date in date_photos:
+        for i, date in enumerate(date_photos):
             for key, value in date.items():
+                print(key,value)
                 if key == 'date':
-                    date_photo = date_photos[n][key]
+                    date_photo = date_photos[i][key]
                     photos[date_photo] = [date_photo]
                 if key == 'sizes':
-                    for all_photo in date_photos[n][key]:
+                    for all_photo in date_photos[i][key]:
                         for key_photo_w, value_photo_w in all_photo.items():
                             if all_photo[key_photo_w] == 'w':
                                 url_photo = all_photo['url']
                 if key == 'likes':
-                    like_photo = date_photos[n][key]['count']
+                    like_photo = date_photos[i][key]['count']
             photos[date_photo] = {'url': url_photo, 'likes': like_photo}
-            n += 1
         return photos
 
     def save_file_json(self):
@@ -58,7 +57,7 @@ class VKAPIClient:
         print('Ссылки фотографий сохранены в url_photo_VK.json')
 
 
-class YANDEX_DISK:
+class YandexDisk:
 
     def __init__(self, token, name_folder='Photo_VK'):
         self.url = 'https://cloud-api.yandex.net/v1/disk/resources'
@@ -81,17 +80,21 @@ class YANDEX_DISK:
         requests.put(url_for_upload, files={'file': response_photo.content})
 
 
-print('Для резервного копирования фотографий с социальной сети В КОНТАКТЕ, на ЯНДЕКС ДИСК необходимо: ')
-TOKEN_VK = input('ввести токен В КОНТАКТЕ: ')
-TOKEN_YANDEX = input('ввести токен ЯНДЕКС ДИСКа: ')
+
+
 
 if __name__ == '__main__':
+    print('Для резервного копирования фотографий с социальной сети В КОНТАКТЕ, на ЯНДЕКС ДИСК необходимо: ')
+    TOKEN_VK = input('ввести токен В КОНТАКТЕ: ')
+    TOKEN_YANDEX = input('ввести токен ЯНДЕКС ДИСКа: ')
+    # TOKEN_VK = 'vk1.a.j1JuaC4V3zSF2y1bE4vsK-o1Y_kvnqeRLvMBOjLq8vsStTb6ZcKxZnBwqBtTKbN4ONhqB9yOU8vS-pGsdCuxrnSbqWvrUqEuV9WMO5qLLtinEQ44j97qs43ROdrJvMZFOixsdtv-yta1q-W0I2yk3xBSXS1pM0KjtaLVoXhk9Yqjb7eaIId4sia-FIqcdBTy'
+    # TOKEN_YANDEX = 'y0_AgAAAAAMTsynAADLWwAAAAEFACEgAADDPPEo4XBLRIDdPGApqHIbErhZcQ'
     vk_client = VKAPIClient(token=TOKEN_VK, user_id=795654478)
     info_user = vk_client.get_info()
     photo_wall = vk_client.photo()
     vk_client.save_file_json()
 
-    save_yd = YANDEX_DISK(token=TOKEN_YANDEX, name_folder='photo_save')
+    save_yd = YandexDisk(token=TOKEN_YANDEX, name_folder='photo_save')
     with open('files/url_photo_VK.json', 'r', encoding='utf-8') as f:
         json_date = json.load(f)
     likes = {}
